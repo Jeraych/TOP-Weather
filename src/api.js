c19f5cf5ae7e57261af7d7ee58b635ddc56ab890
Jeraych <Jacky2022332467@gmail.com>
2026-01-15 11:19:15 +1300

    Return required weather JSON

--- src/api.js
+++ src/api.js
@@ -1,6 +1,17 @@
 export async function getWeather(city) {
     console.log("Fetching weather data");
-    const response = await fetch("");
+    const API_KEY = '54PRMHDBPCVUKVUV2FM9FVFNY';
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?key=" + API_KEY);
     const data = await response.json();
-    return data;
+    const today = data.days[0];
+    return {
+        location: data.address,
+        temp: today.temp,
+        tempMax: today.tempmax,
+        tempMin: today.tempmin,
+        conditions: today.conditions,
+        icon: today.icon,
+        humidity: today.humidity,
+        wind: today.windspeed,
+    };
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { getWeather } from "./api";
 import { renderWeather } from "./dom";
 console.log("Webpack is running");
 
-getWeather("Auckland").then(renderWeather);
+// getWeather("Auckland").then(renderWeather);
+console.log(getWeather("Auckland"));
 console.log("Arranging Auckland weather");
